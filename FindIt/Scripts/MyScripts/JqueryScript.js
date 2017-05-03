923fa9af6a4ec600a346e7e39073e04a5ea50fce
Ivaneev Evgeny <zhe956@gmail.com>
2017-05-03 22:51:07 +0300

    add jqury script for hovering menu elements

---
+++ FindIt/Scripts/MyScripts/JqueryScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".menu_links").hover(
+        function () {
+            $(this).addClass("menu_hover");
+        },
+        function () {
+            $(this).removeClass("menu_hover");
+        });
+    //класс удаляется при обновлении страницы, при сортировке записей по выьранной категории
+    //$(".menu_links").click(function () {
+    //    //$("menu_links").each(function () {
+    //    //    $(this).removeClass("menu_selected");
+    //    //});
+    //    $(this).addClass("menu_selected");
+    //});
+});
